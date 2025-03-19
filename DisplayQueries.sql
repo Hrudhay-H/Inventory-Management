e949f34bf7f4045300b74cb6723229d41b604e87
Hrudhay.H <145639412+Hrudhay-H@users.noreply.github.com>
2025-03-19 20:33:45 +0530

    Create DisplayQueries.sql

---
+++ DisplayQueries.sql
@@ -0,0 +1,17 @@
+desc users;  --Gives the description of the table Users
+select * from users; --Displays the content of the table Users
+
+desc suppliers; --Gives the description of the table Suppliers
+select * from suppliers; --Displays the content of the table Suppliers
+
+desc warehouses; --Gives the description of the table Warehouses
+select * from warehouses; --Displays the content of the table Warehouses
+
+desc products; --Gives the description of the table Products
+select * from products; --Displays the content of the table Products
+
+desc customers; --Gives the description of the table Customers
+select * from customers; --Displays the content of the table Customers
+
+desc orders; --Gives the description of the table Orders
+select * from orders; --Displays the content of the table Orders
